c7f09ee4d8dfefe98d0862a6978c20d354f5c379
Luigi <luismastrangelo@gmail.com>
2019-01-29 12:34:52 +0100

    ReflectiveAccessibility typo.

--- analysis/analysis.r
+++ analysis/analysis.r
@@ -13,7 +13,7 @@ groups <- list(
   'Variance' = c('Clone', 'CovariantReturn', 'RemoveTypeParameter'),
   'Implicit\nTypes' = c('ImplicitIntersectionType', 'ImplicitUnionType'),
   'Structural' = c('SoleSubclassImplementation', 'RecursiveGeneric'),
-  'Reflection' = c('ReflectiveAccesibility', 'NewDynamicInstance'),
+  'Reflection' = c('ReflectiveAccessibility', 'NewDynamicInstance'),
   'Unchecked' = c('UncheckedCast', 'RemoveWildcard', 'GenericArray'),
   'Code Smell' = c('Redundant', 'VariableLessSpecificType', 'RawTypes', 'Literal')
 )
